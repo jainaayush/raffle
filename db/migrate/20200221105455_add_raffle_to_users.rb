08fdbaa9d98a7f01cd29fb89fe892ab65a16ca70
Ayush <ayushgkumar12@gmail.com>
2020-02-21 16:38:16 +0530

    Referenced raffle to user

---
+++ db/migrate/20200221105455_add_raffle_to_users.rb
@@ -0,0 +1,5 @@
+class AddRaffleToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :users, :raffle, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_21_103225) do
+ActiveRecord::Schema.define(version: 2020_02_21_105455) do
 
   create_table "raffles", force: :cascade do |t|
     t.string "name"
@@ -26,7 +26,9 @@ ActiveRecord::Schema.define(version: 2020_02_21_103225) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "raffle_id"
     t.index ["email"], name: "index_users_on_email"
+    t.index ["raffle_id"], name: "index_users_on_raffle_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
